ecd8b4af2aa0228d2e271197ab4d64253a4bfe27
John Burwell <32799077+jmbwell@users.noreply.github.com>
2021-04-03 15:40:23 -0500

    python 2.7

--- getUpsData.py
+++ getUpsData.py
@@ -13,7 +13,7 @@ string_measurements=["battery.mfr.date", "battery.type", "battery.date", "device
             "ups.beeper.status", "ups.mfr","ups.model", "ups.serial", "ups.status", "ups.test.result", "ups.firmware", "ups.firmware.aux","ups.mfr.date", "ups.productid",
             "driver.parameter.port", "driver.parameter.syncronous"]
 
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.FNULL)
 
 for line in p.stdout.readlines(): #read and store result in log file
     line = line.decode("utf-8").rstrip()
